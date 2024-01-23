0824937541db4b1f5583f6e9d0f8f5af688e41f4
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-01-24 00:00:53 +0530

    api data added

--- src/GetMeme.js
+++
@@ -1,8 +0,0 @@
-export default function GetMeme(){
-    const memeArr = fetch("https://api.imgflip.com/get_memes").then(f=>{
-        console.log(memeArr);
-    })
-    return (
-        <img src="images/troll-face.png" alt="img" />
-    );
-}
--- src/Meme.js
+++ src/Meme.js
@@ -1,21 +1,26 @@
 import React from "react";
-import memesData from "./memesData.js";
-
 
 export default function Meme() {
+  // const [memesData, setMemesData] = React.useState([]);   #API
+  
+
   const [meme, setMeme] = React.useState({
     topText:"", 
     bottomText:"", 
     randomImage: "https://i.imgflip.com/30b1gx.jpg"
     });
 
-  const [allMemes, setAllMemes] = React.useState(memesData.data.memes);
+  const [allMemes, setAllMemes] = React.useState([]);
 
+  React.useEffect(()=>{
+    fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+  },[])
 
   function getMemeImage() {
-    const memesArray = allMemes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
-    const memeImage = memesArray[randomNumber].url;
+    const randomNumber = Math.floor(Math.random() * allMemes.length)
+    const memeImage = allMemes[randomNumber].url;
     setMeme(prevState => {
         return {
             ...prevState,
@@ -32,7 +37,7 @@ export default function Meme() {
         [event.target.name]: event.target.value,
       })
     })
-    console.log(meme);
+
   }
 
     return (
